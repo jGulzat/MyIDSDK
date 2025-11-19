9b08206475d63afe1bbf5fbde23b1c344abcb168
gzheenbekkyzy <gzheenbekkyzy@nurtelecom.kg>
2025-11-19 15:57:08 +0600

    added sdk package

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "MyIdSDK",
+    platforms: [ .iOS(.v12) ],
+    products: [
+        .library(
+            name: "MyIdSDK",
+            type: .dynamic,
+            targets: ["MyIdSDK"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MyIdSDK",
+            url: "https://github.com/jGulzat/MyID/releases/download/1.0.0/MyIdSDK.xcframework.zip",
+            checksum: "37e6073071a3ab2ed1f2b5e8e93c4a456912bd479716839ccdaa96a84f9db036"
+        )
+    ]
+)
