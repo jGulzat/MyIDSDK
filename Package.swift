a212dd65886a5b1a352d8f3f64f32d0e393b3d77
gzheenbekkyzy <gzheenbekkyzy@nurtelecom.kg>
2025-11-19 16:47:36 +0600

    new package

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MyIdSDK",
-            url: "https://github.com/jGulzat/MyIDSDK/releases/download/1.0.1/MyIdSDK.xcframework.zip",
+            url: "https://github.com/jGulzat/MyIDSDK/releases/download/1.0.2/MyIdSDK.xcframework.zip",
             checksum: "37e6073071a3ab2ed1f2b5e8e93c4a456912bd479716839ccdaa96a84f9db036"
         )
     ]
